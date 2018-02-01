f0f0d1086c6350c37f237ec9301ccfaf278afce8
Carl Johnson <me@carlmjohnson.net>
2018-02-01 15:39:56 -0500

    Add local templates

---
+++ serve.sh
@@ -0,0 +1,11 @@
+set -eux -o pipefail
+
+# Get the directory that this script file is in
+THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+cd "$THIS_DIR"
+
+venv-datasette/bin/datasette serve \
+    --template-dir ./templates \
+    --static static:./static \
+    data/*.db
