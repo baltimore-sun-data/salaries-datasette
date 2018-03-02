61849d6f69856bbae8286f26827498cdb1f3f686
Carl Johnson <me@carlmjohnson.net>
2018-03-02 14:33:46 -0600

    JS: Add js-active class ASAP to prevent flash of unstyled content

--- static/js/app.v1.js
+++ static/js/app.v1.js
@@ -6,8 +6,6 @@ NodeList.prototype.addEventListener = function(event, func) {
 };
 
 window.addEventListener("load", function() {
-  document.body.classList.add("js-active");
-
   document
     .querySelectorAll(".icon-twitter.js-click")
     .addEventListener("click", function(e) {
