c6ea72bd804b9e59bb42a740cbb500d4eb81ef7e
Carl Johnson <me@carlmjohnson.net>
2019-01-09 14:54:06 -0500

    Frontend: Simplify social buttons

--- frontend/src/js/social.js
+++ frontend/src/js/social.js
@@ -1,39 +1,35 @@
-import { each } from "./utils.js";
+import { on } from "./utils.js";
 
-each(".icon-twitter.js-click", el => {
-  el.addEventListener("click", () => {
-    var tweet = el.getAttribute("data-share-text");
-    var url = el.getAttribute("data-url");
-    if (!url) {
-      url = window.location.href;
-    }
+on("click", ".icon-twitter.js-click", e => {
+  var tweet = e.currentTarget.getAttribute("data-share-text");
+  var url = e.currentTarget.getAttribute("data-url");
+  if (!url) {
+    url = window.location.href;
+  }
 
-    var twitterURL =
-      "https://twitter.com/intent/tweet?text=" +
-      encodeURIComponent(tweet) +
-      "&url=" +
-      encodeURIComponent(url) +
-      "&tw_p=tweetbutton";
-    window.open(twitterURL, "_blank", "width=500,height=300,toolbar=no");
-    return false;
-  });
+  var twitterURL =
+    "https://twitter.com/intent/tweet?text=" +
+    encodeURIComponent(tweet) +
+    "&url=" +
+    encodeURIComponent(url) +
+    "&tw_p=tweetbutton";
+  window.open(twitterURL, "_blank", "width=500,height=300,toolbar=no");
+  return false;
 });
 
-each(".icon-facebook.js-click", el => {
-  el.addEventListener("click", () => {
-    // FaceBook has deprecated all the options in the pop-up,
-    // so it all needs to be controlled by the meta tags on the page.
-    // See https://developers.facebook.com/docs/sharing/reference/feed-dialog
-    var url = el.getAttribute("data-url");
-    if (!url) {
-      url = window.location.href;
-    }
+on("click", ".icon-facebook.js-click", e => {
+  // FaceBook has deprecated all the options in the pop-up,
+  // so it all needs to be controlled by the meta tags on the page.
+  // See https://developers.facebook.com/docs/sharing/reference/feed-dialog
+  var url = e.currentTarget.getAttribute("data-url");
+  if (!url) {
+    url = window.location.href;
+  }
 
-    var facebookURL =
-      "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link=" +
-      encodeURIComponent(url) +
-      "&redirect_uri=https://www.facebook.com";
-    window.open(facebookURL, "_blank", "width=500,height=500,toolbar=no");
-    return false;
-  });
+  var facebookURL =
+    "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link=" +
+    encodeURIComponent(url) +
+    "&redirect_uri=https://www.facebook.com";
+  window.open(facebookURL, "_blank", "width=500,height=500,toolbar=no");
+  return false;
 });
